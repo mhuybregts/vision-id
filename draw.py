b5a3dbcbe20ad5331693a9f90d866b9c8b475ce2
mhuybregts <123901369+mhuybregts@users.noreply.github.com>
2024-06-19 18:38:52 -0400

    feat: Create Draw class

---
+++ draw.py
@@ -0,0 +1,51 @@
+import cv2
+
+FRAME_THICKNESS = 4
+FONT_THICKNESS = 1
+BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+
+
+class Draw:
+
+    def __init__(self, color, frame_thickness, font_thickness):
+        self.color = color
+        self.frame_thickness = frame_thickness
+        self.font_thickness = font_thickness
+
+    def draw_rectangle(self, image, top_left, bottom_right, color, filled):
+        if filled:
+            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+        else:
+            cv2.rectangle(image, top_left, bottom_right, color,
+                          self.frame_thickness)
+
+    def add_text(self, image, text, origin, color):
+        cv2.putText(image, text, origin, cv2.FONT_HERSHEY_SIMPLEX, 0.5, color,
+                    self.font_thickness)
+
+    def to_bgr(image):
+        return cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+
+if __name__ == "__main__":
+
+    draw = Draw(BLACK, FRAME_THICKNESS, FONT_THICKNESS)
+    image = cv2.imread("test/draw_test.jpg")
+
+    # NOTE: tl = Top Left, br = Bottom Right
+    padding = FRAME_THICKNESS//2
+    tl1, br1 = (250, 250), (500, 500)
+    tl2, br2 = (250 - padding, 500), (500 + padding, 500 + 22)
+    origin = (250 + 10, 500 + 15)
+
+    # Test draw functions
+    image = Draw.to_bgr(image)
+    draw.draw_rectangle(image, tl1, br1, BLACK, filled=False)
+    draw.draw_rectangle(image, tl2, br2, BLACK, filled=True)
+    draw.add_text(image, "Press any key", origin, WHITE)
+
+    # Show images
+    cv2.imshow("TEST", image)
+    cv2.waitKey(0)
+    cv2.destroyWindow("TEST")
--- face_rec_photo.py
+++ face_rec_photo.py
@@ -2,8 +2,8 @@ import face_recognition
 import os
 import cv2
 
-KNOWN_FACES_DIR = "known_faces"
-UNKNOWN_FACES_DIR = "unknown_faces"
+KNOWN_FACES_DIR = "test/known_faces"
+UNKNOWN_FACES_DIR = "test/unknown_faces"
 TOLERANCE = 0.6  # Default value is 0.6
 FRAME_THICKNESS = 3
 FONT_THICKNESS = 2
